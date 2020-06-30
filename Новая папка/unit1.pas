6a5f96b6b3ffdd3e34bce68a8a3e290bd5f36895
Danil Pestov <danilca.pestoff@gmail.com>
2020-06-30 08:05:52 +0300

    third commit

--- Новая папка/unit1.pas
+++ Новая папка/unit1.pas
@@ -30,6 +30,13 @@ type
     Label5: TLabel;
     Label6: TLabel;
     Label7: TLabel;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+    procedure Button4Click(Sender: TObject);
+    procedure Button5Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure Label6Click(Sender: TObject);
   private
 
   public
@@ -43,5 +50,58 @@ implementation
 
 {$R *.lfm}
 
+{ TForm1 }
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  close;
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+ var
+   a,b :real;
+begin
+    a:= StrToFloat(Edit1.Text);
+    b:=a*0.62137119223733 ;
+    Edit4.Clear;
+  Edit4. Text:=FloatToStr(b);
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+begin
+  Label7.Caption:='Пестов Данил(ИР1930)';
+end;
+
+procedure TForm1.Button4Click(Sender: TObject);
+   var
+     a,b : real;
+begin
+    a:= StrToFloat(Edit2.Text);
+    b:=a*1000/3600 ;
+    Edit5.Clear;
+  Edit5. Text:=FloatToStr(b);
+end;
+
+procedure TForm1.Button5Click(Sender: TObject);
+    var
+     a,b : real;
+begin
+    a:= StrToFloat(Edit3.Text);
+    b:=a*2.2369362920544 ;
+    Edit6.Clear;
+  Edit6.Text:=FloatToStr(b);
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.Label6Click(Sender: TObject);
+begin
+
+end;
+
 end.
+
 
